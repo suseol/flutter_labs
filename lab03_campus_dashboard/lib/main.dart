dabc2b2e2fb9aeb8d7319e7b7218d2311fd404e4
suseol <61640778+suseol@users.noreply.github.com>
2026-09-23 03:50:23 +0900

    Add instructional comments in main.dart
    Added comments to guide users through the app development process.

--- lab03_campus_dashboard/lib/main.dart
+++ lab03_campus_dashboard/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 // 이번 실습은 처음부터 차근차근 앱을 완성해 갑니다.
 // 중간에 놓치면 github에서 중간버전을 받아서 진행하도록 하세요
+// 아래 코드는 main()에서 수행하기 때문에 Hot Reload가 안됩니다. 
+// 실습 중 MaterialApp()을 build()에서 리턴하도록 수정하게 됩니다.
 
 void main() {
   runApp(
