4afa7ec9d57e09e530f2a3e6a6e8eee5b725fc83
suseol <61640778+suseol@users.noreply.github.com>
2026-09-23 02:42:49 +0900

    Initialize campus dashboard app with basic structure

---
+++ lab03_campus_dashboard/lib/main_sbs.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+// 이번 실습은 처음부터 차근차근 앱을 완성해 갑니다.
+// 중간에 놓치면 github에서 중간버전을 받아서 진행하도록 하세요
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('캠퍼스 대시보드 앱'),
+        ),
+      ),
+    ),
+  );
+}
